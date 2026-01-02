f2aacfe46e29f68cea804c6e3a32f8d3e3fb1e4b
Md Tahsin Rahman <tahsin.rahman@agoda.com>
2026-01-02 18:24:32 +0800

    nvim: autosave on edit

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -7,3 +7,20 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+-- Auto-save after a short delay when text changes
+local autosave_timer = nil
+vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+  pattern = "*",
+  callback = function()
+    if autosave_timer then
+      autosave_timer:stop()
+    end
+    autosave_timer = vim.defer_fn(function()
+      if vim.bo.modified and vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
+        vim.cmd("silent! write")
+      end
+    end, 1000) -- 1 second delay
+  end,
+  desc = "Auto-save after delay",
+})
+
