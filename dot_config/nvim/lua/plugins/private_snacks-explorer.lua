6d3d1c6cf228f436a4a9be5aa2d1b481d5668d47
Md Tahsin Rahman <tahsin.rahman@agoda.com>
2026-01-02 13:39:22 +0800

    nvim: open file explorer by default

--- dot_config/nvim/lua/plugins/private_snacks-explorer.lua
+++ dot_config/nvim/lua/plugins/private_snacks-explorer.lua
@@ -1,12 +1,24 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    explorer = {
+      replace_netrw = true,
+    },
     picker = {
       sources = {
         explorer = {
           hidden = true,
+          auto_close = false,
         },
       },
     },
   },
+  init = function()
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VeryLazy",
+      callback = function()
+        Snacks.explorer()
+      end,
+    })
+  end,
 }
