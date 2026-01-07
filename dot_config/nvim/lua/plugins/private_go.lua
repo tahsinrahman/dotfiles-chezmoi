64973c49c5e4be0c01b24ff8ec45358a37e163bc
Tahsin Rahman <tahsin8243@gmail.com>
2026-01-07 22:37:13 +0800

    nvim: go - disable staticcheck

---
+++ dot_config/nvim/lua/plugins/private_go.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              staticcheck = false,
+            },
+          },
+        },
+      },
+    },
+  },
+}
