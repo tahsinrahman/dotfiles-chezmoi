5bd6bd328a8e413399c3ce7abe61ba417999922d
Tahsin Rahman <tahsin8243@gmail.com>
2026-01-07 22:36:50 +0800

    nvim: enable s/S to replace+insert char/line

---
+++ dot_config/nvim/lua/plugins/private_flash.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "folke/flash.nvim",
+    keys = {
+      { "s", mode = { "n", "x", "o" }, false }, -- disable s mapping
+      { "S", mode = { "n", "x", "o" }, false }, -- disable S mapping
+    },
+  },
+}
