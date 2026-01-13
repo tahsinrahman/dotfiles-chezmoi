e410047e50c7035d76d5cc428fa93884b101d72a
Md Tahsin Rahman <tahsin.rahman@agoda.com>
2026-01-13 14:51:46 +0800

    nvim: use biome for json formatting

--- dot_config/nvim/lua/plugins/private_formatting.lua
+++ dot_config/nvim/lua/plugins/private_formatting.lua
@@ -1,18 +1,24 @@
 return {
   {
     "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        json = { "prettier" },
-        yaml = { "prettier" },
-      },
-    },
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.json = { "biome" }
+      opts.formatters_by_ft.yaml = { "prettier" }
+      opts.formatters = opts.formatters or {}
+      opts.formatters.biome = {
+        env = { BIOME_CONFIG_PATH = os.getenv("HOME") .. "/.config/biome" },
+        args = { "format", "--stdin-file-path", "$FILENAME" },
+      }
+      return opts
+    end,
   },
   {
     "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "prettier",
+        "biome",
       },
     },
   },
