acc30805c53e4dd8738d487a2822b7f4a887ead2
Md Tahsin Rahman <tahsin.rahman@agoda.com>
2026-02-02 01:27:24 +0800

    nvim: go don't show type hints

--- dot_config/nvim/lua/plugins/private_go.lua
+++ dot_config/nvim/lua/plugins/private_go.lua
@@ -7,6 +7,15 @@ return {
           settings = {
             gopls = {
               staticcheck = false,
+              hints = {
+                assignVariableTypes = false,
+                compositeLiteralFields = false,
+                compositeLiteralTypes = false,
+                constantValues = false,
+                functionTypeParameters = false,
+                parameterNames = false,
+                rangeVariableTypes = false,
+              },
             },
           },
         },
