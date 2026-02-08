fdeaee7f2e0a3e1cc0b0cd2db27ea0c4c96793b9
Tahsin Rahman <tahsin8243@gmail.com>
2026-02-08 17:56:05 +0800

    nvim: fix typescript

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- dot_config/nvim/lua/plugins/private_formatting.lua
+++ dot_config/nvim/lua/plugins/private_formatting.lua
@@ -5,6 +5,10 @@ return {
       opts.formatters_by_ft = opts.formatters_by_ft or {}
       opts.formatters_by_ft.json = { "biome" }
       opts.formatters_by_ft.yaml = { "prettier" }
+      opts.formatters_by_ft.typescript = { "prettier" }
+      opts.formatters_by_ft.typescriptreact = { "prettier" }
+      opts.formatters_by_ft.javascript = { "prettier" }
+      opts.formatters_by_ft.javascriptreact = { "prettier" }
       opts.formatters = opts.formatters or {}
       opts.formatters.biome = {
         env = { BIOME_CONFIG_PATH = os.getenv("HOME") .. "/.config/biome" },
