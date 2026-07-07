42e5e298110d8505330592541a373ab1bbe8f3d3
Tahsin Rahman <tahsin8243@gmail.com>
2026-07-07 22:19:07 +0800

    Find keyboard backlight controls by label

--- run_once_after_25-keyboard-backlight.sh
+++ run_once_after_25-keyboard-backlight.sh
@@ -16,18 +16,52 @@ delay 2
 tell application "System Events"
   tell process "System Settings"
     set frontmost to true
+    my clickOff("Adjust keyboard brightness in low light", window 1)
+    my setSliderToZero("Keyboard brightness", window 1)
+  end tell
+end tell
 
+on clickOff(targetName, rootElement)
+  tell application "System Events"
     try
-      if value of checkbox "Adjust keyboard brightness in low light" of group 1 of group 2 of splitter group 1 of group 1 of window 1 is 1 then
-        click checkbox "Adjust keyboard brightness in low light" of group 1 of group 2 of splitter group 1 of group 1 of window 1
+      if (name of rootElement as text) is targetName then
+        try
+          if value of rootElement is 1 then click rootElement
+          return true
+        end try
       end if
     end try
 
     try
-      set value of slider "Keyboard brightness" of group 1 of group 2 of splitter group 1 of group 1 of window 1 to 0
+      repeat with childElement in UI elements of rootElement
+        if my clickOff(targetName, childElement) then return true
+      end repeat
     end try
   end tell
-end tell
+  return false
+end clickOff
+
+on setSliderToZero(targetName, rootElement)
+  tell application "System Events"
+    try
+      if (role of rootElement as text) is "AXSlider" then
+        try
+          if (name of rootElement as text) contains targetName then
+            set value of rootElement to 0
+            return true
+          end if
+        end try
+      end if
+    end try
+
+    try
+      repeat with childElement in UI elements of rootElement
+        if my setSliderToZero(targetName, childElement) then return true
+      end repeat
+    end try
+  end tell
+  return false
+end setSliderToZero
 APPLESCRIPT
 then
   echo "Could not set keyboard backlight automatically. Grant Accessibility to Terminal/Codex and rerun this script."
